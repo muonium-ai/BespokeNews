d52daeb7a94d503c626fae2cda603030737b5a26
senthil nayagam <senthil@muonium.ai>
2024-11-04 09:51:23 +0530

    removed duplicated code

--- bn_app.py
+++ bn_app.py
@@ -141,54 +141,6 @@ def markdown_filter(text):
     return Markup(clean_html)
 
 
-def get_db_connection():
-    current_date = datetime.now().strftime("%d_%m_%Y")
-    db_name = f"./db/hackernews_{current_date}.db"
-    conn = sqlite3.connect(db_name)
-    conn.row_factory = sqlite3.Row  # Enable column access by name
-    return conn
-
-
-def fetch_news_items(query=None, order_by=None):
-    """Fetch news items from the database, optionally filtering by a search query."""
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    if query:
-        cursor.execute(
-            """
-            SELECT id, title, by, url, score, content, summary, priority 
-            FROM stories
-            WHERE title LIKE ?
-            ORDER BY priority DESC, score DESC
-        """,
-            ("%" + query + "%",),
-        )
-    else:
-        if order_by:
-            cursor.execute(f"""
-                SELECT id, title, by, url, score, content, summary, priority
-                FROM stories
-                ORDER BY {order_by} DESC, priority DESC, score DESC
-            """)
-        else:
-            cursor.execute("""
-                SELECT id, title, by, url, score, content, summary, priority
-                FROM stories
-                 ORDER BY priority DESC, score DESC
-            """)
-    news_items = cursor.fetchall()
-    conn.close()
-    return news_items
-
-
-def filter_news_items(news_items):
-    """Filter news items based on the blacklist."""
-    filtered_news = [
-        item
-        for item in news_items
-        if not blacklist.is_blacklisted(item["url"], item["title"])
-    ]
-    return filtered_news
 
 
 @app.before_request
